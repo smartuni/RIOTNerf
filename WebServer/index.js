73bb8a01e24b754a35ef65b89416ba0163055410
SCArtis <jerom.schult@gmx.de>
2016-12-19 14:32:58 +0100

    Webserver handles TargetInput. Addresses LAsers directly per Id

--- WebServer/index.js
+++ WebServer/index.js
@@ -45,18 +45,27 @@ var coapServer = coap.createServer({type: 'udp6'});
 ////////////////////////////GLOBAL-FIELDS//////////////////////////
 
 //Die IPs fuer die IOT-Geraete bleiben auch bei Neustart gleich!!
-var coapLaser1 = 'fd1d:8d5c:12a5:0:585a:6a65:70bd:247e';
-var coapLaser2 = 'fd1d:8d5c:12a5:0:5841:1644:2407:f2c2';
+var coapLaser1 = 'fd1d:8d5c:12a5:0:585a:6a65:70bd:247e%lowpan0';
+var coapLaser2 = 'fd1d:8d5c:12a5:0:5841:1644:2407:f2c2%lowpan0';
+		
+
+//die Ips der Zielscheiben
+var target1 = 'fe80::5855:5f70:108b:c07e'
+var target2 = 'fe80::5841:174a:589e:f29e'	
+var target1hit = false;
+var target2hit = false;								
 
 //Multicast-Adresse
 var coapPath = 'ff02::1%lowpan0';
 //direkte Ansprache der Laser momentan (noch) nicht moeglich
+/*
 coapLaser1 = coapPath;
 coapLaser2 = coapPath;
-
+*/
 
 //Globale Variablen fuer Game-Logic...
 var userCount = 0;
+
 var input1Count = 0;
 var input2Count = 0;
 var collectiveHorizontal1 = 0;
@@ -64,6 +73,8 @@ var collectiveVertical1 = 0;
 var collectiveHorizontal2 = 0;
 var collectiveVertical2 = 0;
 
+
+var laserState = [0,0];
 
 
 
@@ -140,23 +151,45 @@ io.on('connection', function(socket){
 		setLaser(msg);
 	});
 
-	//Steuerung ueber WASD
-	socket.on('keyPressed', function(msg){
-		console.log('KeyPressed');
+	//Steuerung ueber Pfeiltasten
+	socket.on('keyPressed1', function(msg){
+		//console.log('KeyPressed');
 		if(msg == 'left'){
 			console.log('left pressed');
-			servosNSteps(coapPath, '-1 0');
+			servosNSteps(coapLaser1, '-1 0');
 		} else if(msg == 'right'){
 			console.log('right pressed');
-			servosNSteps(coapPath, '1 0');
+			servosNSteps(coapLaser1, '1 0');
 		} else if(msg == 'up'){
 			console.log('up pressed');
-			servosNSteps(coapPath, '0 1');
+			servosNSteps(coapLaser1, '0 1');
 		} else if(msg == 'down'){
 			console.log('down pressed');
-			servosNSteps(coapPath, '0 -1');
-		} else if(msg == 'space'){
+			servosNSteps(coapLaser1, '0 -1');
+		} else if(msg == 'fire'){
+			console.log('fire2 pressed');
 			fireLaser(1);
+		}
+	});
+
+	//TODO: WASD
+	socket.on('keyPressed2', function(msg){
+		//console.log('KeyPressed2');
+		if(msg == 'left'){
+			console.log('left2 pressed');
+			servosNSteps(coapLaser2, '-1 0');
+		} else if(msg == 'right'){
+			console.log('right2 pressed');
+			servosNSteps(coapLaser2, '1 0');
+		} else if(msg == 'up'){
+			console.log('up2 pressed');
+			servosNSteps(coapLaser2, '0 1');
+		} else if(msg == 'down'){
+			console.log('down2 pressed');
+			servosNSteps(coapLaser2, '0 -1');
+		} else if(msg == 'fire'){
+			console.log('fire2 pressed');
+			fireLaser(2);
 		}
 	});
 });
@@ -179,8 +212,31 @@ http.listen(3000, function(){
 //TODO: Antwort auf Zielscheibe
 coapServer.on('request', function(req, res){
   console.log('COAP: Request came in');
-  req.pipe(process.stdout);
+
+
+  // message.rsinfo, könnte Infos zur richtigen IP geben
+
+  var payload = req.payload;
+  var payloadString = payload.toString();
+  console.log(payloadString)
+
+  var info = req.rsinfo;
+  var address = info.address;
+  console.log(address.toString());
+  if((address.toString() === target1) && !target1hit){
+  	console.log("Zielscheibe1 wurde getroffen!");
+  	target1hit = true;
+  }
+
+  if((address.toString() === target2) && !target2hit){
+  	console.log("Zielscheibe2 wurde getroffen!");
+  	target2hit = true;
+  }
+
+  io.emit('LaserTrigger', 'leeeeel');
+
   res.end();
+
   /*var method = req.method;
   console.log(method);
   var url = req.url;
@@ -293,9 +349,20 @@ function round(){
 function fireLaser(num){
 	var laserPath = '/periph/laser';
 	if(num == 1){
-		coapPut(coapLaser1, laserPath, '1');
+		switchLaserState(0);
+		coapPut(coapLaser1, laserPath, ''+ laserState[0]);
 	} else {
-		coapPut(coapLaser2, laserPath, '1');
+		switchLaserState(1);
+		coapPut(coapLaser2, laserPath, ''+ laserState[1]);
+	}
+}
+
+function switchLaserState(laser){
+	var currLaserState = laserState[laser];
+	if(currLaserState == 0){
+		laserState[laser] = 1;
+	} else{
+		laserState[laser] = 0;
 	}
 }
 
